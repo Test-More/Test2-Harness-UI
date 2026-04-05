f55abf078e353aedc55439511f839254b330e5f0
Chad Granum <exodist7@gmail.com>
2026-04-05 11:12:10 -0700

    Automated Version Bump

--- lib/App/Yath/Command/ui.pm
+++ lib/App/Yath/Command/ui.pm
@@ -2,7 +2,7 @@ package App::Yath::Command::ui;
 use strict;
 use warnings;
 
-our $VERSION = '0.000146';
+our $VERSION = '0.000147';
 
 use Test2::Util qw/pkg_to_file/;
 
--- lib/Test2/Harness/UI/Controller/Project.pm
+++ lib/Test2/Harness/UI/Controller/Project.pm
@@ -2,7 +2,7 @@ package Test2::Harness::UI::Controller::Project;
 use strict;
 use warnings;
 
-our $VERSION = '0.000146';
+our $VERSION = '0.000147';
 
 use Time::Elapsed qw/elapsed/;
 use List::Util qw/sum/;
